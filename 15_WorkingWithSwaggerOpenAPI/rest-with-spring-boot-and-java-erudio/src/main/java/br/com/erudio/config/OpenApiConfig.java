b7042d39080632ea7db38bc14e4e662d35ea4558
leandrocgsi <leandrocgsi@gmail.com>
2024-12-23 13:48:05 -0300

    Adding Swagger Open API Support

--- 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/OpenApiConfig.java
+++ 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/OpenApiConfig.java
@@ -1,29 +1,26 @@
 package br.com.erudio.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
-	
-	@Bean
-	OpenAPI customOpenAPI() {
-		return new OpenAPI()
-			.info(new Info()
-				.title("REST API's RESTful from 0 with Java 21, Spring Boot 3.4.0, Kubernetes and Docker")
-				.version("v1")
-				.description("Some description about your API")
-				.termsOfService("https://pub.erudio.com.br/meus-cursos")
-				.license(
-					new License()
-						.name("Apache 2.0")
-						.url("https://pub.erudio.com.br/meus-cursos")
-					)
-				);
-	}
 
+    @Bean
+    OpenAPI customOpenAPI(){
+        return new OpenAPI()
+            .info(new Info()
+                .title("REST API's RESTful from 0 with Java, Spring Boot, Kubernetes and Docker")
+                    .version("v1")
+                    .description("REST API's RESTful from 0 with Java, Spring Boot, Kubernetes and Docker")
+                    .termsOfService("https://pub.erudio.com.br/meus-cursos")
+                    .license(new License()
+                        .name("Apache 2.0")
+                        .url("https://pub.erudio.com.br/meus-cursos")
+                    )
+            );
+    }
 }
--- 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/docs/BookControllerDocs.java
+++ 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/docs/BookControllerDocs.java
@@ -1,6 +1,6 @@
 package br.com.erudio.controllers.docs;
 
-import br.com.erudio.data.dto.PersonDTO;
+import br.com.erudio.data.dto.BookDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -8,15 +8,16 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-public interface PersonControllerDocs {
+public interface BookControllerDocs {
 
-    @Operation(summary = "Find All People",
-            description = "Finds All People",
-            tags = {"People"},
+    @Operation(summary = "Find All Book",
+            description = "Finds All Book",
+            tags = {"Book"},
             responses = {
                     @ApiResponse(
                             description = "Success",
@@ -24,7 +25,7 @@ public interface PersonControllerDocs {
                             content = {
                                     @Content(
                                             mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                            array = @ArraySchema(schema = @Schema(implementation = PersonDTO.class))
+                                            array = @ArraySchema(schema = @Schema(implementation = BookDTO.class))
                                     )
                             }),
                     @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
@@ -34,16 +35,16 @@ public interface PersonControllerDocs {
                     @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
             }
     )
-    List<PersonDTO> findAll();
+    List<BookDTO> findAll();
 
-    @Operation(summary = "Finds a Person",
-            description = "Find a specific person by your ID",
-            tags = {"People"},
+    @Operation(summary = "Finds a Book",
+            description = "Find a specific book by your ID",
+            tags = {"Book"},
             responses = {
                     @ApiResponse(
                             description = "Success",
                             responseCode = "200",
-                            content = @Content(schema = @Schema(implementation = PersonDTO.class))
+                            content = @Content(schema = @Schema(implementation = BookDTO.class))
                     ),
                     @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
                     @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
@@ -52,32 +53,32 @@ public interface PersonControllerDocs {
                     @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
             }
     )
-    PersonDTO findById(@PathVariable("id") Long id);
+    BookDTO findById(@PathVariable("id") Long id);
 
-    @Operation(summary = "Adds a new Person",
-            description = "Adds a new person by passing in a JSON, XML or YML representation of the person.",
-            tags = {"People"},
+    @Operation(summary = "Adds a new Book",
+            description = "Adds a new book by passing in a JSON, XML or YML representation of the book.",
+            tags = {"Book"},
             responses = {
                     @ApiResponse(
                             description = "Success",
                             responseCode = "200",
-                            content = @Content(schema = @Schema(implementation = PersonDTO.class))
+                            content = @Content(schema = @Schema(implementation = BookDTO.class))
                     ),
                     @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
                     @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
                     @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
             }
     )
-    PersonDTO create(@RequestBody PersonDTO person);
+    BookDTO create(@RequestBody BookDTO book);
 
-    @Operation(summary = "Updates a person's information",
-            description = "Updates a person's information by passing in a JSON, XML or YML representation of the updated person.",
-            tags = {"People"},
+    @Operation(summary = "Updates a book's information",
+            description = "Updates a book's information by passing in a JSON, XML or YML representation of the updated book.",
+            tags = {"Book"},
             responses = {
                     @ApiResponse(
                             description = "Success",
                             responseCode = "200",
-                            content = @Content(schema = @Schema(implementation = PersonDTO.class))
+                            content = @Content(schema = @Schema(implementation = BookDTO.class))
                     ),
                     @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
                     @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
@@ -86,11 +87,11 @@ public interface PersonControllerDocs {
                     @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
             }
     )
-    PersonDTO update(@RequestBody PersonDTO person);
+    BookDTO update(@RequestBody BookDTO book);
 
-    @Operation(summary = "Deletes a Person",
-            description = "Deletes a specific person by their ID",
-            tags = {"People"},
+    @Operation(summary = "Deletes a Book",
+            description = "Deletes a specific book by their ID",
+            tags = {"Book"},
             responses = {
                     @ApiResponse(
                             description = "No Content",
