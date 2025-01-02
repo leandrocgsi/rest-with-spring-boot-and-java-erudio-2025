05ef345726f1d5fb8fc2be219eb85049a793b487
leandrocgsi <leandrocgsi@gmail.com>
2025-01-02 00:33:57 -0300

    Generating Professional Reports with JasperReports and Spring Boot - V2

--- 22_WorkingWithJasperReports/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/file/exporter/impl/PdfExporter.java
+++ 22_WorkingWithJasperReports/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/file/exporter/impl/PdfExporter.java
@@ -11,8 +11,6 @@ import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,9 +21,9 @@ public class PdfExporter implements FileExporter {
     @Override
     public FileExportResponse exportFile(List<PersonDTO> people) throws Exception {
         // Obtenha o arquivo de template Jasper
-        InputStream inputStream = getClass().getResourceAsStream("/templates/People.jrxml");
+        InputStream inputStream = getClass().getResourceAsStream("/templates/people.jrxml");
         if (inputStream == null) {
-            throw new RuntimeException("Template file not found: /templates/People.jrxml");
+            throw new RuntimeException("Template file not found: /templates/people.jrxml");
         }
 
         // Compile o template Jasper, se necessário
@@ -34,18 +32,13 @@ public class PdfExporter implements FileExporter {
         // Preencha os dados no template
         JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(people);
         Map<String, Object> parameters = new HashMap<>();
-        //parameters.put("title", "People Report"); // Adicione parâmetros ao template, se necessário
+        parameters.put("title", "People Report"); // Adicione parâmetros ao template, se necessário
 
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
 
         // Exporte o relatório para um array de bytes
         try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
             JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
-
-            // Salvar o arquivo na unidade D://Code
-            Path outputPath = Path.of("D://Code/people_exported.pdf");
-            Files.write(outputPath, outputStream.toByteArray());
-
             Resource resource = new ByteArrayResource(outputStream.toByteArray());
             return new FileExportResponse(resource, "people_exported.pdf");
         }
