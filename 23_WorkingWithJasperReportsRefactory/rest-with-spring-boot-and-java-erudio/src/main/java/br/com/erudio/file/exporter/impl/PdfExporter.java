f91e43bcc1e15b5f72c1a0d9ee3061a77b7e56bb
leandrocgsi <leandrocgsi@gmail.com>
2025-01-04 01:19:00 -0300

    Generating Professional Reports with JasperReports and Spring Boot

--- 23_WorkingWithJasperReportsRefactory/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/file/exporter/impl/PdfExporter.java
+++ 23_WorkingWithJasperReportsRefactory/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/file/exporter/impl/PdfExporter.java
@@ -1,7 +1,7 @@
 package br.com.erudio.file.exporter.impl;
 
 import br.com.erudio.data.dto.PersonDTO;
-import br.com.erudio.file.exporter.contract.FileExporter;
+import br.com.erudio.file.exporter.contract.PersonExporter;
 import br.com.erudio.services.QRCodeService;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
@@ -18,13 +18,13 @@ import java.util.List;
 import java.util.Map;
 
 @Component
-public class PdfExporter implements FileExporter {
+public class PdfExporter implements PersonExporter {
 
     @Autowired
     QRCodeService qrCodeService;
 
     @Override
-    public Resource exportFile(List<PersonDTO> people) throws Exception {
+    public Resource exportPeople(List<PersonDTO> people) throws Exception {
         InputStream inputStream = getClass().getResourceAsStream("/templates/people.jrxml");
         if (inputStream == null) {
             throw new RuntimeException("Template file not found: /templates/people.jrxml");
@@ -45,31 +45,23 @@ public class PdfExporter implements FileExporter {
 
     @Override
     public Resource exportPerson(PersonDTO person) throws Exception {
-        // Carrega o template principal
         InputStream mainTemplateStream = getClass().getResourceAsStream("/templates/person.jrxml");
         if (mainTemplateStream == null) {
             throw new RuntimeException("Template file not found: /templates/person.jrxml");
         }
 
-        // Carrega o template do subrelatório
         InputStream subReportStream = getClass().getResourceAsStream("/templates/books.jrxml");
         if (subReportStream == null) {
             throw new RuntimeException("Subreport file not found: /templates/books.jrxml");
         }
 
-        // Compila o subrelatório
         JasperReport subReport = JasperCompileManager.compileReport(subReportStream);
-
-        // Compila o relatório principal
         JasperReport mainReport = JasperCompileManager.compileReport(mainTemplateStream);
 
-        // Gera o QR Code e passa como parâmetro
         InputStream qrCodeStream = qrCodeService.createQRCode(person.getProfileUrl(), 200, 200);
 
-        // Configura o DataSource do subrelatório
         JRBeanCollectionDataSource subReportDataSource = new JRBeanCollectionDataSource(person.getBooks());
 
-        // Configura os parâmetros para o relatório principal
         String path = getClass().getResource("/templates/books.jasper").getPath();
 
         Map<String, Object> parameters = new HashMap<>();
@@ -78,14 +70,11 @@ public class PdfExporter implements FileExporter {
         parameters.put("SUB_REPORT_DIR", path);
         parameters.put("QR_CODE_IMAGE", qrCodeStream); // Passa o QR Code como parâmetro
 
-        // Configura o DataSource do relatório principal
         JRBeanCollectionDataSource mainReportDataSource = new JRBeanCollectionDataSource(
                 Collections.singletonList(person));
 
-        // Preenche o relatório principal
         JasperPrint jasperPrint = JasperFillManager.fillReport(mainReport, parameters, mainReportDataSource);
 
-        // Exporta o relatório como PDF e retorna como recurso
         try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
             JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
             return new ByteArrayResource(outputStream.toByteArray());
