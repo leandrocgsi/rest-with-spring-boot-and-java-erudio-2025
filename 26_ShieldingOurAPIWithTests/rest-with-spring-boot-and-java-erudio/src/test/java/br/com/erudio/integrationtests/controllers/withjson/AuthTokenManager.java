b6366d5611455749ab2cbe5115a7ae5c311489f1
leandrocgsi <leandrocgsi@gmail.com>
2025-01-08 11:30:10 -0300

    Shielding our API with Rest Assured, Testcontainers and JUnit 5

---
+++ 26_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integrationtests/controllers/withjson/AuthTokenManager.java
@@ -0,0 +1,31 @@
+package br.com.erudio.integrationtests.controllers.withjson;
+
+import br.com.erudio.config.TestConfigs;
+import br.com.erudio.integrationtests.dto.AccountCredentialsDTO;
+import br.com.erudio.integrationtests.dto.TokenDTO;
+
+import static io.restassured.RestAssured.given;
+
+public class AuthTokenManager {
+
+    private static TokenDTO tokenDTO;
+
+    public static TokenDTO getToken(String contentType) {
+        if (tokenDTO == null) {
+            AccountCredentialsDTO user = new AccountCredentialsDTO("leandro", "admin123");
+            tokenDTO = given()
+                .basePath("/auth/signin")
+                .port(TestConfigs.SERVER_PORT)
+                .contentType(contentType)
+                .body(user)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(TokenDTO.class);
+        }
+        return tokenDTO;
+    }
+}
--- 26_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integrationtests/controllers/withjson/PersonControllerJsonTest.java
+++ 26_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integrationtests/controllers/withjson/PersonControllerJsonTest.java
@@ -1,7 +1,9 @@
 package br.com.erudio.integrationtests.controllers.withjson;
 
 import br.com.erudio.config.TestConfigs;
+import br.com.erudio.integrationtests.dto.AccountCredentialsDTO;
 import br.com.erudio.integrationtests.dto.PersonDTO;
+import br.com.erudio.integrationtests.dto.TokenDTO;
 import br.com.erudio.integrationtests.dto.wrappers.json.WrapperPersonDTO;
 import br.com.erudio.integrationtests.testcontainers.AbstractIntegrationTest;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -37,6 +39,34 @@ class PersonControllerJsonTest extends AbstractIntegrationTest {
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
         person = new PersonDTO();
+    }
+
+    @Test
+    @Order(0)
+    public void authorization() {
+        AccountCredentialsDTO user = new AccountCredentialsDTO("leandro", "admin123");
+
+        var accessToken = given()
+                .basePath("/auth/signin")
+                .port(TestConfigs.SERVER_PORT)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(user)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(TokenDTO.class)
+                .getAccessToken();
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + accessToken)
+                .setBasePath("/api/person/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
     }
 
     @Test
@@ -271,6 +301,8 @@ class PersonControllerJsonTest extends AbstractIntegrationTest {
         person.setLastName("Torvalds");
         person.setAddress("Helsinki - Finland");
         person.setGender("Male");
+        person.setPhotoUrl("https://pub.erudio.com.br/meus-cursos");
+        person.setProfileUrl("https://pub.erudio.com.br/meus-cursos");
         person.setEnabled(true);
     }
 }
